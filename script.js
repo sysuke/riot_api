6963b74a6176cc704aa918a3555ccd7ff35c1c2d
shiki <shiki@www7270uf.sakura.ne.jp>
2016-02-05 13:26:48 +0900

    get summoner ID

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 
 function check()
 {
-//	document.write();
+	//	document.write();
 	var d_nerf = document.getElementById( 'nerf' );
-	var d_appkey = document.getElementById( 'appkey' );
+	var name =document.form1.field1.value ;
+	var summpnerID ;
 
-	d_nerf.insertAdjacentHTML( 'beforeend', document.form1.field1.value );
-	d_appkey.insertAdjacentHTML( 'beforeend', APP_KEY );
+	$.ajax({
+		url: 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + name+ '?api_key=' + API_KEY,
+		type: 'GET',
+		dataType: 'json',
+		data: {
+
+		},
+		success: function (json) {
+			var SUMMONER_NAME_NOSPACES = name.replace(" ", "");
+
+			SUMMONER_NAME_NOSPACES = SUMMONER_NAME_NOSPACES.toLowerCase().trim();
+
+			summonerLevel = json[SUMMONER_NAME_NOSPACES].summonerLevel;
+			summonerID = json[SUMMONER_NAME_NOSPACES].id;
+
+
+			sumName = json[SUMMONER_NAME_NOSPACES].name;
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert("error getting Summoner data!");
+		}
+	});
+	d_nerf.insertAdjacentHTML( 'beforeend', summonerID );
+
 }
