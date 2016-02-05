ca4844d73787a5a704a72a5267d6e2d55531bd4f
shiki <shiki@www7270uf.sakura.ne.jp>
2016-02-05 10:56:06 +0900

    iadd app_key file

---
+++ app_key.js
@@ -0,0 +1,1 @@
+var APP_KEY = "your app key"
--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 function check()
 {
-	alert("nerf plz " + document.form1.field1.value);
+//	document.write();
+	var d_nerf = document.getElementById( 'nerf' );
+	var d_appkey = document.getElementById( 'appkey' );
+
+	d_nerf.insertAdjacentHTML( 'beforeend', document.form1.field1.value );
+	d_appkey.insertAdjacentHTML( 'beforeend', APP_KEY );
 }
